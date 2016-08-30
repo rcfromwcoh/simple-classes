19e14f7c4706492eb15a9d50c3c430e845205818
Robert Horrocks <rhorrocks@bds.local>
2016-08-30 16:25:18 -0400

    first commit

---
+++ WorkingWithSimpleClasses/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithSimpleClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            //myCar.Make = "Pontiac";
+            //myCar.Model = "Bonneville";
+            //myCar.Year = 1977;
+            //myCar.Color = "Red";
+
+            
+
+            //Console.WriteLine("{0} {1} {2} {3}", myCar.Make, myCar.Model, myCar.Year, myCar.Color);
+            //decimal value = DetermineMarketValue(myCar);
+            //Console.WriteLine("{0:C}", value);
+
+            //Console.WriteLine("{0:C}", myCar.DetermineMarketValue());
+
+            Console.ReadLine();
+        }
+        //private static decimal DetermineMarketValue(Car car)
+        //{
+        //    decimal carValue = 100.0m;
+        //    return carValue;
+        //}
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        //public decimal DetermineMarketValue()
+        //{
+        //    decimal carValue;
+
+        //    if (Year > 1990)
+        //        carValue = 10000;
+        //    else
+        //        carValue = 2000;
+
+        //    return carValue;
+        //}
+    }
+}
